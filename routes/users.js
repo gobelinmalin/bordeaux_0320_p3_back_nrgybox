1a2e4c2dc910eff9908b0fdf8818d6b9c2480d72
Bouchelot <bouchelot.m@gmail.com>
2020-06-02 17:47:44 +0200

    fix

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
-// const connection = require("../config");
+const connection = require("../config");
 
 // Test
 router.get("/", (req, res) => {
-  console.log("ok");
+  connection.query("SELECT * from user", (err, results) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json(results);
+    }
+  });
 });
-
+// ! test
 module.exports = router;
